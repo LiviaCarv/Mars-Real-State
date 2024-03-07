1d71041dfec0522724788ffb712af81827fd3a1f
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-03-07 17:56:54 -0400

    added DetailFragment, its view model and factory

---
+++ app/src/main/java/com/project/marsrealstate/detail/DetailFragment.kt
@@ -0,0 +1,21 @@
+package com.project.marsrealstate.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.project.marsrealstate.R
+import com.project.marsrealstate.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentDetailBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+        return binding.root
+    }
+
+}
